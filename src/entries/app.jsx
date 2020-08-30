e5918d2595ecf89a6031473a303f631dc2338541
ryosuketter <r.shimizu.yale@gmail.com>
2020-08-30 12:56:12 +0900

    routing エントリーポイントの実装 ページ遷移を実装

---
+++ src/entries/app.jsx
@@ -0,0 +1,10 @@
+import "core-js/stable";
+import "regenerator-runtime/runtime";
+import React from "react";
+import ReactDOM from "react-dom";
+
+import App from "~/routings/App";
+
+const rootEl = document.getElementById("root");
+
+ReactDOM.render(<App />, rootEl);
--- webpack.config.babel.js
+++ webpack.config.babel.js
@@ -1,22 +1,30 @@
-import webpack from 'webpack';
-import path from 'path';
+import webpack from "webpack";
+import path from "path";
 
 export default (env, args) => {
-  const isProduction = args.mode === 'production';
-  const devtool = !isProduction && 'inline-source-map';
+  const isProduction = args.mode === "production";
+  const devtool = !isProduction && "inline-source-map";
   const rules = [
     {
       test: /\.jsx?$/,
-      use: ['babel-loader'],
+      use: ["babel-loader"],
     },
   ];
 
   return {
-    entry: './src/entries/sample.jsx',
+    devtool,
+    entry: "./src/entries/app.jsx",
     output: {
-      path: path.join(__dirname, './output/'),
-      filename: 'sample.js',
+      path: path.join(__dirname, "./public/js/"),
+      filename: "app.js",
     },
     module: { rules },
+    resolve: {
+      modules: ["node_modules"],
+      alias: {
+        "~": path.join(__dirname, "./src/"),
+      },
+      extensions: [".js", ".jsx"],
+    },
   };
 };
