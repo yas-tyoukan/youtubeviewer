5c0bbc5a6f282b665666038628982d75b3c99703
yas-tyoukan <uuaauo@gmail.com>
2020-09-25 20:35:31 +0900

    6-2 お気に入り機能のサーバー実装

--- api/index.js
+++ api/index.js
@@ -2,6 +2,9 @@ const express = require("express");
 const { google } = require("googleapis");
 const REACT_APP_YOUTUBE_API_KEY = "AIzaSyCCVbaUVJRZ23dTiekEQWD1qrluis3YOPs";
 
+// 追加する
+const { readFavoriteIds, writeFavoriteIds } = require('../utils/favorite');
+
 const youtube = google.youtube({
   version: "v3",
   auth: REACT_APP_YOUTUBE_API_KEY,
@@ -32,6 +35,26 @@ router.get("/videos/search/:keyword", (req, res, next) => {
       id: ids.join(","),
     });
     res.json({ items, nextPageToken });
+  })().catch(next);
+});
+
+// 追加する
+// お気に入り動画の取得
+router.get('/videos/favorites', (req, res, next) => {
+  (async () => {
+    // お気に入り動画IDを取得
+    const favoriteIds = await readFavoriteIds();
+    if (!favoriteIds.length) {
+      // お気に入りが1つもなければ空配列を返す
+      res.json({ items: [] });
+      return;
+    }
+    // お気に入りのIDから動画の取得
+    const { data: { items } } = await youtube.videos.list({
+      part: 'statistics,snippet',
+      id: favoriteIds.join(','),
+    });
+    res.json({ items });
   })().catch(next);
 });
 
@@ -77,5 +100,49 @@ router.get("/videos/:videoId/related", (req, res, next) => {
     res.json({ items, nextPageToken });
   })().catch(next);
 });
+
+// 追加する
+// お気に入り動画ID一覧取得
+router.get('/favorites', (req, res, next) => {
+  readFavoriteIds().then((data) => {
+    res.json(data);
+  }).catch(next);
+});
+
+// 追加する
+// お気に入り登録・解除
+router.route('/favorites/:id')
+  // お気に入り登録
+  .post((req, res, next) => {
+    (async () => {
+      const { id } = req.params;
+      // 現時点のお気に入りリストを読み込み
+      const favoriteIds = await readFavoriteIds();
+      if (favoriteIds.indexOf(id) === -1) {
+        // パラメータに指定されたIDが現時点のお気に入りになかった場合
+        // お気に入りリストに追加
+        favoriteIds.unshift(id);
+        // お気に入りリストを書き込む
+        writeFavoriteIds(favoriteIds);
+      }
+      res.end();
+    })().catch(next);
+  })
+  // お気に入り解除
+  .delete((req, res, next) => {
+    (async () => {
+      const { id } = req.params;
+      // 現時点のお気に入りリストを読み込み
+      const favoriteIds = await readFavoriteIds();
+      const indexOfId = favoriteIds.indexOf(id);
+      if (indexOfId !== -1) {
+        // パラメータに指定されたIDが現時点のお気に入りにあった場合
+        // 指定されたIDを削除したものをお気に入りリストに書き込む
+        writeFavoriteIds(favoriteIds.filter((favoriteId) => (favoriteId !== id)));
+      }
+      res.end();
+    })().catch(next);
+  });
+
 
 module.exports = router;
